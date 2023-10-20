61d9bf031bdb1237693739bf438c3fecfcf21930
Ismail Ali <ismailchangezi801@gmail.com>
2023-10-20 17:51:20 +0500

    Style the hero section

---
+++ frontend/src/Components/Hero/Hero.js
@@ -0,0 +1,26 @@
+import React from "react";
+import hand_icon from "../Assets/hand_icon.png";
+import arrow_icon from "../Assets/arrow.png";
+import hero_image from "../Assets/hero_image.png";
+export const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-left">
+        <h2>NEW ARRIVALS ONLY</h2>
+        <div className="hand-hand-icon">
+          <p>new</p>
+          <img src={hand_icon} alt="" />
+        </div>
+        <p>collections</p>
+        <p>for everyone</p>
+      </div>
+      <div className="hero-latest-btn">
+        <div>Latest Collection</div>
+        <img src={arrow_icon} alt="hero icon" />
+      </div>
+      <div className="hero-right">
+        <img src={hero_image} alt="hero image" />
+      </div>
+    </div>
+  );
+};
