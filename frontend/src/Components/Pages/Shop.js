c22e816e61e269947d651ae6bce90e724f1f5a4a
Ismail Ali <ismailchangezi801@gmail.com>
2023-10-24 19:01:29 +0500

    Render the items on the Shop Page

--- frontend/src/Components/Pages/Shop.js
+++ frontend/src/Components/Pages/Shop.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { Hero } from "../Hero/Hero";
+import { Popular } from "../Popular/Popular";
 
 const Shop = () => {
-  return <Hero />;
+  return (
+    <div>
+      <Hero />
+      <Popular />
+    </div>
+  );
 };
 
 export default Shop;
